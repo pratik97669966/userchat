c7ea524f1f13cd65541f4ea77da0396bbf0a52b1
Pratik Pansare <pratik97669966@gmail.com>
2022-08-07 17:06:06 +0530

    a

--- server.js
+++ server.js
@@ -1,48 +1,37 @@
-/* eslint-disable no-console */
-/* eslint-disable import/no-unresolved */
-const path = require('path');
-const express = require('express');
-
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const socket = require('socket.io');
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
-const io = socket(server);
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
 
-const PID = process.pid;
-const PORT = process.env.PORT || 3000;
-const users = {};
-let usersNum = 0;
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 
-// eslint-disable-next-line no-shadow
-io.on('connection', (socket) => {
-  console.log(`The socket is connected! Socket id: ${socket.id}`);
-  usersNum += 1;
-
-  socket.on('new-user', (username) => {
-    io.emit('broadcast', `Online: ${usersNum}`);
-    users[socket.id] = username;
-    socket.broadcast.emit('user-connected', username);
-  });
-
-  socket.on('new-message', (data) => {
-    io.emit('new-message', data);
-  });
-
-  socket.on('is-typing', (username) => {
-    socket.broadcast.emit('is-typing', username);
-  });
-
-  socket.on('disconnect', () => {
-    usersNum -= 1;
-    io.emit('broadcast', `Online: ${usersNum}`);
-    socket.broadcast.emit('user-disconnected', users[socket.id]);
-    delete users[socket.id];
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`The server is Listening on http://localhost:${PORT} \nPID: ${PID}\n`);
-});
+server.listen(process.env.PORT || 3030);
