a7ec1c55201d4c90ea18481c5c7639d40c2d13ea
Pratik Pansare <pratik97669966@gmail.com>
2022-08-08 21:01:29 +0530

    j

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
-const showChat = document.querySelector("#showChat");
+// const showChat = document.querySelector("#showChat");
 // const backBtn = document.querySelector(".header__back");
 const usersCounter = document.getElementById('users-counter');
 // const inviteButton = document.querySelector("#inviteButton");
@@ -18,11 +18,11 @@ document.querySelector(".main__right").style.display = "flex";
 //   document.querySelector(".header__back").style.display = "none";
 // });
 
-showChat.addEventListener("click", () => {
-  document.querySelector(".main__right").style.display = "flex";
-  document.querySelector(".main__right").style.flex = "1";
-  document.querySelector(".header__back").style.display = "block";
-});
+// showChat.addEventListener("click", () => {
+//   document.querySelector(".main__right").style.display = "flex";
+//   document.querySelector(".main__right").style.flex = "1";
+//   document.querySelector(".header__back").style.display = "block";
+// });
 const params = new URLSearchParams(window.location.search);
 const user = params.get('userName');
 
