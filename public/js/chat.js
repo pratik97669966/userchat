e93f01807078e326df606e894f909aeeadedb036
Pratik Pansare <pratik.p@qupapp.com>
2023-03-12 16:28:35 +0530

    t

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,99 +1,71 @@
-// eslint-disable-next-line no-undef
-const socket = io.connect('/');
+// // eslint-disable-next-line no-undef
+// const socket = io.connect('/');
 
-const message = document.getElementById('message-input');
-const sendMsg = document.getElementById('send-message');
-const msgSound = document.getElementById('notification-sound');
-const user = document.getElementById('username-input');
-const displayMsg = document.getElementById('display-message');
-const typingLabel = document.getElementById('typing-label');
-const chatWindow = document.getElementById('chat-window');
-const usersCounter = document.getElementById('users-counter');
-const msgErr = document.getElementById('message-error');
-// const join = document.getElementById('you-joined');
-const chat = document.getElementById('chat');
+// const message = document.getElementById('message-input');
+// const sendMsg = document.getElementById('send-message');
+// const msgSound = document.getElementById('notification-sound');
+// const user = document.getElementById('username-input');
+// const displayMsg = document.getElementById('display-message');
+// const typingLabel = document.getElementById('typing-label');
+// const chatWindow = document.getElementById('chat-window');
+// const usersCounter = document.getElementById('users-counter');
+// const msgErr = document.getElementById('message-error');
+// // const join = document.getElementById('you-joined');
+// const chat = document.getElementById('chat');
 
-var userName = "";
-var userNametrs = "";
+// var userName = "";
+// var userNametrs = "";
 
-const params = new URLSearchParams(window.location.search);
-userName = params.get('userName').slice(0, -28);
-chat.style.display = 'block';
-// join.innerHTML = '<p>Welcome ' + userName + '<p>';
-socket.emit('new-user', userName);
+// const params = new URLSearchParams(window.location.search);
+// userName = params.get('userName').slice(0, -28);
+// chat.style.display = 'block';
+// // join.innerHTML = '<p>Welcome ' + userName + '<p>';
+// socket.emit('new-user', userName);
 
+// sendMsg.addEventListener('click', () => {
 
-function checkMatch(userMessage) {
+//   if (message.value === null || message.value.trim().length === 0) {
+//     msgErr.innerHTML = '🚨 Message is required!';
+//     return;
+//   }
 
-  let originalMessage = userMessage;
-  let inputMessage = userMessage.toLowerCase();
-  let result = inputMessage.match(/(asshole|ass hole|fuck off|fuck you|sucking|gspot|fuck|fuckoff|fuckface|fuckface|ass|cumbubble|bugger|cumdumpsterfuck|cocknose|wanker|fuck you|bollocks|shitbag|knobhead|twatwaffle|shit|choad|thundercunt|pissoff|bitch|tits|dickhead|knobjockey|asshole|crikey|shitpouch|cuntpuddle|dickweed|rubbish|jizzstain|dickweasel|cunt|pissflaps|nonce|quim|bitch|shag|pisskidney|bawbag|fuck|trumpet|bastard)/g);
-  //  var slangLengh = result.length;
-  //document.getElementById("matchResult").innerHTML = result;
-  console.log(result);
-  if (result != null) {
-    //Copy message to support team.
-    return 1;
-  } else {
-    return 0;
-  }
-}
-sendMsg.addEventListener('click', () => {
-
-  if (message.value === null || message.value.trim().length === 0) {
-
-    msgErr.innerHTML = '🚨 Message is required!';
-    return;
-  }
-  let chresult = checkMatch(message.value);
-  let systemMessage = "";
-  if (chresult == 1) {
-    // systemMessage="<br/><span style='color: red;'>🚨 Using bad word may ban your account permanantly</span>";
-  }
-  socket.emit('new-message', {
-
-    message: message.value + systemMessage,
-    username: userName,
-  });
-  message.value = '';
-  msgErr.innerHTML = '';
-});
-
-// message.addEventListener('keypress', () => {
-//   socket.emit('is-typing', userName);
+//   socket.emit('new-message', {
+//     message: message.value + systemMessage,
+//     username: userName,
+//   });
+//   message.value = '';
+//   msgErr.innerHTML = '';
 // });
 
+// socket.on('user-connected', (username) => {
+//   displayMsg.innerHTML += `<p><strong>${username}</strong> has connected!</p>`;
+//   chatWindow.scrollTop = chatWindow.scrollHeight;
+// });
 
-socket.on('user-connected', (username) => {
-  displayMsg.innerHTML += `<p><strong>${username}</strong> has connected!</p>`;
-  chatWindow.scrollTop = chatWindow.scrollHeight;
+// socket.on('broadcast', (number) => {
+//   usersCounter.innerHTML = number;
+// });
+
+// socket.on('new-message', (data) => {
+//   typingLabel.innerHTML = '';
+//   displayMsg.innerHTML += `<p><strong>${data.username}</strong> : ${data.message}</p>`;
+//   chatWindow.scrollTop = chatWindow.scrollHeight;
   
-});
+// });
 
-socket.on('broadcast', (number) => {
-  usersCounter.innerHTML = number;
-});
+// socket.on('is-typing', (username) => {
+//   typingLabel.innerHTML = `<p>${username} is typing...</p>`;
+//   chatWindow.scrollTop = chatWindow.scrollHeight;
+// });
 
-socket.on('new-message', (data) => {
-  typingLabel.innerHTML = '';
-  displayMsg.innerHTML += `<p><strong>${data.username}</strong> : ${data.message}</p>`;
-  chatWindow.scrollTop = chatWindow.scrollHeight;
-  
-});
-
-socket.on('is-typing', (username) => {
-  typingLabel.innerHTML = `<p>${username} is typing...</p>`;
-  chatWindow.scrollTop = chatWindow.scrollHeight;
-});
-
-socket.on('user-disconnected', (username) => {
-  if (username == null) {
-    displayMsg.innerHTML += '<p>Unlogged user has disconnected!</p>';
-    chatWindow.scrollTop = chatWindow.scrollHeight;
-  } else {
-    displayMsg.innerHTML += `<p><strong>${username}</strong> has disconnected!</p>`;
-    chatWindow.scrollTop = chatWindow.scrollHeight;
+// socket.on('user-disconnected', (username) => {
+//   if (username == null) {
+//     displayMsg.innerHTML += '<p>Unlogged user has disconnected!</p>';
+//     chatWindow.scrollTop = chatWindow.scrollHeight;
+//   } else {
+//     displayMsg.innerHTML += `<p><strong>${username}</strong> has disconnected!</p>`;
+//     chatWindow.scrollTop = chatWindow.scrollHeight;
     
-  }
-});
+//   }
+// });
 
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-console */
-/* eslint-disable import/no-unresolved */
 const path = require('path');
 const express = require('express');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const server = require('http').Server(app);
@@ -11,38 +10,73 @@ const io = socket(server);
 
 const PID = process.pid;
 const PORT = process.env.PORT || 5000;
-const users = {};
-let usersNum = 0;
 
-app.use(express.static(path.join(__dirname, 'public')));
+const mongoURI = 'mongodb://mongo:5XGOcoMpsJX6xGk9nnsH@containers-us-west-175.railway.app:7581';
 
-// eslint-disable-next-line no-shadow
-io.on('connection', (socket) => {
-  console.log(`The socket is connected! Socket id: ${socket.id}`);
-  usersNum += 1;
+// Connect to MongoDB
+MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((client) => {
+    console.log('Connected to MongoDB');
 
-  socket.on('new-user', (username) => {
-    io.emit('broadcast', `Online: ${usersNum}`);
-    users[socket.id] = username;
-    socket.broadcast.emit('user-connected', username);
+    const db = client.db('myapp');
+    const usersCollection = db.collection('users');
+
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    // Send the list of all users to the connected client
+    const sendUsersList = (socket) => {
+      usersCollection.find().toArray()
+        .then((users) => {
+          socket.emit('users-list', users);
+        })
+        .catch((err) => {
+          console.log('Error getting users from MongoDB:', err);
+        });
+    };
+
+    // eslint-disable-next-line no-shadow
+    io.on('connection', (socket) => {
+      console.log(`The socket is connected! Socket id: ${socket.id}`);
+
+      // Send the list of all users to the new client
+      sendUsersList(socket);
+
+      // Add a new user to the database
+      socket.on('new-user', (user) => {
+        usersCollection.insertOne(user)
+          .then(() => {
+            console.log(`User ${user.name} added to MongoDB`);
+            sendUsersList(io);
+          })
+          .catch((err) => {
+            console.log('Error adding user to MongoDB:', err);
+          });
+      });
+
+      socket.on('is-typing', (username) => {
+        socket.broadcast.emit('is-typing', username);
+      });
+
+      // Remove a user from the database
+      socket.on('disconnect', () => {
+        usersCollection.findOneAndDelete({ id: socket.id })
+          .then((result) => {
+            const user = result.value;
+            if (user) {
+              console.log(`User ${user.name} removed from MongoDB`);
+              sendUsersList(io);
+            }
+          })
+          .catch((err) => {
+            console.log('Error removing user from MongoDB:', err);
+          });
+      });
+    });
+
+    server.listen(PORT, () => {
+      console.log(`The server is Listening on http://localhost:${PORT} \nPID: ${PID}\n`);
+    });
+  })
+  .catch((err) => {
+    console.log('Error connecting to MongoDB:', err);
   });
-
-  socket.on('new-message', (data) => {
-    io.emit('new-message', data);
-  });
-
-  socket.on('is-typing', (username) => {
-    socket.broadcast.emit('is-typing', username);
-  });
-
-  socket.on('disconnect', () => {
-    usersNum -= 1;
-    io.emit('broadcast', `Online: ${usersNum}`);
-    socket.broadcast.emit('user-disconnected', users[socket.id]);
-    delete users[socket.id];
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`The server is Listening on http://localhost:${PORT} \nPID: ${PID}\n`);
-});
