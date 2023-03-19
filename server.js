90b727931156d6dddd45c500a0b5c598f843403f
Pratik Pansare <pratik.p@qupapp.com>
2023-03-19 22:10:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,22 +3,11 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server, {
-  maxHttpBufferSize: 1e8, // Increase max HTTP buffer size to handle large payloads
-  transports: ['websocket'], // Use only the websocket transport for better performance
-  pingTimeout: 60000, // Increase the ping timeout to prevent disconnects due to slow network
-  pingInterval: 25000, // Decrease the ping interval for faster heartbeat checks
-  upgradeTimeout: 30000, // Increase the upgrade timeout to handle slow upgrades
-  allowUpgrades: true, // Allow upgrades to websocket transport
-  cors: { origin: '*' }, // Allow all CORS requests
-  cookie: false, // Disable cookie support
-  perMessageDeflate: { // Enable per-message deflate compression
-    threshold: 1024,
-  },
-  httpCompression: true, // Enable HTTP compression
+const io = require('socket.io')(server, { 
+  pingInterval: 10000, 
+  pingTimeout: 5000,
+  maxHttpBufferSize: 1e8,
 });
-
- io.sockets.setMaxListeners(5000);
 
 const PID = process.pid;
 const PORT = process.env.PORT || 5000;
