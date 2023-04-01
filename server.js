bee96f629318a68bdac77149a384df9991d90c8f
Pratik Pansare <pratik.p@qupapp.com>
2023-04-01 17:21:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const { MongoClient } = require('mongodb');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {
@@ -9,269 +8,86 @@ const io = require('socket.io')(server, {
   maxHttpBufferSize: 1e8,
 });
 io.sockets.setMaxListeners(5000);
-const mydata = [
-  {
-    "name": "Sneha",
-    "isReadyToTalk": false,
-    "isVerified": false,
-    "opentalks": 4382,
-    "rating": 9474,
-    "profile": "https://picsum.photos//200?sig=",
-    "gender": "MALE",
-    "talkingmin": 3823,
-    "uId": "fdhasfkhljpjodgvjndglvkdlgvkjnd",
-    "description": "I love to play soccer and travel to new places. Looking to connect with others who share similar interests.",
-    "token": "test",
-    "username": "sneha46"
-  },
-  {
-    "name": "Lila",
-    "isReadyToTalk": false,
-    "isVerified": true,
-    "opentalks": 2498,
-    "rating": 8542,
-    "profile": "https://picsum.photos//200?sig=",
-    "gender": "FEMALE",
-    "talkingmin": 1392,
-    "uId": "fdhasfkhljpjodgvjndglvkdlhjkjnd",
-    "description": "I am an artist who enjoys painting and sketching. Looking for someone to talk to about art and maybe exchange ideas.",
-    "token": "test",
-    "username": "lila32"
-  },
-  {
-    "name": "John",
-    "isReadyToTalk": true,
-    "isVerified": true,
-    "opentalks": 1276,
-    "rating": 7635,
-    "profile": "https://picsum.photos//200?sig=",
-    "gender": "MALE",
-    "talkingmin": 924,
-    "uId": "fdhasfkhljpjodgvjndghjkdlgvkjnd",
-    "description": "I'm an entrepreneur and love discussing business and startups. Let's chat and share ideas.",
-    "token": "test",
-    "username": "johnny"
-  },
-  {
-    "name": "Sophie",
-    "isReadyToTalk": true,
-    "isVerified": true,
-    "opentalks": 2893,
-    "rating": 9312,
-    "profile": "https://picsum.photos//200?sig=",
-    "gender": "FEMALE",
-    "talkingmin": 1867,
-    "uId": "fdhasfkhljpjodgghjndglvkdlgvkjnd",
-    "description": "I'm a foodie and love trying out new recipes. Looking for someone to share recipes and cooking tips with.",
-    "token": "test",
-    "username": "sophielovesfood"
-  },
-  {
-    "name": "Sam",
-    "isReadyToTalk": false,
-    "isVerified": false,
-    "opentalks": 982,
-    "rating": 6243,
-    "profile": "https://picsum.photos//200?sig=",
-    "gender": "MALE",
-    "talkingmin": 539,
-    "uId": "fdhasfkhljpjodhjhjndglvkdlgvkjnd",
-    "description": "I'm a music lover and enjoy playing guitar. Looking for someone to chat with about music and maybe jam together.",
-    "token": "test",
-    "username": "sammyguitar"
-  },
-  {
-    "name": "Maria",
-    "isReadyToTalk": true,
-    "isVerified": false,
-    "opentalks": 302,
-    "rating": 4215,
-    "profile": "https://picsum.photos//200?sig=",
-    "gender": "FEMALE",
-    "talkingmin": 239,
-    "uId": "fdhasfkhljpjodgjkjndglvkdlgvkjnd",
-    "description": "I'm a fitness enthusiast and enjoy working out. Looking for someone to motivate me and maybe workout together.",
-    "token": "test",
-    "username": "mariafit"
-  },
-  {
-    "name": "William",
-    "isReadyToTalk": true,
-    "isVerified": true,
-    "opentalks": 564,
-    "rating": 7856,
-    "profile": "https://picsum.photos//200?sig=",
-    "gender": "MALE",
-    "talkingmin": 342,
-    "uId": "fdhasfkhljpjoghjgvjndglvkdlgvkjnd",
-    "description": "I'm a traveler and enjoy exploring new places. Looking for someone to share travel stories and maybe plan a trip together.",
-    "token": "test",
-    "username": "williamtravels"
-  },
-  {
-    "name": "Emma",
-    "isReadyToTalk": true,
-    "isVerified": true,
-    "opentalks": 1483,
-    "rating": 9123,
-    "profile": "https://picsum.photos//200?sig=",
-    "gender": "FEMALE",
-    "talkingmin": 943,
-    "uId": "fdhasfkhljpjoghgvjndglvkdlgvkjnd",
-    "description": "I'm a bookworm and love reading. Looking for someone to talk about books and maybe exchange recommendations.",
-    "token": "test",
-    "username": "emma_reads"
-  },
-  {
-    "name": "Alex",
-    "isReadyToTalk": false,
-    "isVerified": false,
-    "opentalks": 712,
-    "rating": 6752,
-    "profile": "https://picsum.photos//200?sig=",
-    "gender": "MALE",
-    "talkingmin": 423,
-    "uId": "fdhasfkhljpjodovjndglvkdlgvkjnd",
-    "description": "I'm a gamer and enjoy playing video games. Looking for someone to chat about games and maybe play together.",
-    "token": "test",
-    "username": "alex_gaming"
-  },
-  {
-    "name": "Olivia",
-    "isReadyToTalk": true,
-    "isVerified": true,
-    "opentalks": 3275,
-    "rating": 8765,
-    "profile": "https://picsum.photos//200?sig=",
-    "gender": "FEMALE",
-    "talkingmin": 2398,
-    "uId": "fdhasfigljpjodgvjndglvkdlgvkjnd",
-    "description": "I'm a movie buff and love watching films. Looking for someone to talk about movies and maybe watch together.",
-    "token": "test",
-    "username": "oliviamoviebuff"
-  }
 
-]
-const connectedUsers = [];
 const PID = process.pid;
 const PORT = process.env.PORT || 5000;
 
-const mongoURI = 'mongodb://mongo:5XGOcoMpsJX6xGk9nnsH@containers-us-west-175.railway.app:7581';
+console.log('Deployment Done !!');
+app.use(express.static(path.join(__dirname, 'public')));
+const users = [];
+const availableUsers = {
+  any: [],
+  male: [],
+  female: []
+};
+const matchTimeout = 2 * 60 * 1000; // 2 minutes in milliseconds
 
-// Connect to MongoDB
-MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((client) => {
-    console.log('Connected to MongoDB');
+io.on('connection', socket => {
+  // When a user connects to the server
+  console.log('User connected');
 
-    const db = client.db('myapp');
-    const usersCollection = db.collection('users');
-
-    app.use(express.static(path.join(__dirname, 'public')));
-
-    // Send the list of all users to the connected client
-    const sendUsersList = (socket) => {
-      usersCollection.find().toArray()
-        .then((users) => {
-          socket.emit('users-list', users);
-        })
-        .catch((err) => {
-          console.log('Error getting users from MongoDB:', err);
-        });
-    };
-
-    // eslint-disable-next-line no-shadow
-    io.on('connection', (socket) => {
-      console.log(`The socket is connected! Socket id: ${socket.id}`);
-
-      // Send the list of all users to the new client
-      connectedUsers[socket.id] = socket;
-      sendUsersList(socket);
-
-      // Add a new user to the database
-      socket.on('new-user', (user) => {
-        user.id = socket.id;
-        usersCollection.insertOne(user)
-          .then(() => {
-            console.log(`User ${user.name} added to MongoDB`);
-            io.emit('user-add', user);
-          })
-          .catch((err) => {
-            console.log('Error adding user to MongoDB:', err);
-          });
-      });
-      socket.on('insert-all', (users) => {
-
-        const socketId = socket.id;
-        const usersWithSocketId = mydata.map((user) => ({ ...user, id: socketId }));
-
-        usersCollection.insertMany(usersWithSocketId)
-          .then(() => {
-            console.log(`${usersWithSocketId.length} users added to MongoDB`);
-            sendUsersList(io);
-          })
-          .catch((err) => {
-            console.log('Error adding users to MongoDB:', err);
-          });
-      });
-      socket.on('is-typing', (username) => {
-        socket.broadcast.emit('is-typing', username);
-      });
-      socket.on('private-message', (message) => {
-        const recipientSocket = connectedUsers[message.id];
-        if (recipientSocket) {
-          recipientSocket.emit('private-message', message);
-        } else {
-          console.log('Recipient socket not found');
-        }
-      });
-      socket.on('private-btnRequestAccept', (message) => {
-        const recipientSocket = connectedUsers[message.id];
-        if (recipientSocket) {
-          recipientSocket.emit('private-btnRequestAccept', message);
-        } else {
-          console.log('Recipient socket not found');
-        }
-      });
-      socket.on('private-btnRequestCancel', (message) => {
-        const recipientSocket = connectedUsers[message.id];
-        if (recipientSocket) {
-          recipientSocket.emit('private-btnRequestCancel', message);
-        } else {
-          console.log('Recipient socket not found');
-        }
-      });
-      socket.on('private-btnCancel', (message) => {
-        const recipientSocket = connectedUsers[message.id];
-        if (recipientSocket) {
-          recipientSocket.emit('private-btnCancel', message);
-        } else {
-          console.log('Recipient socket not found');
-        }
-      });
-      // Remove a user from the database
-      socket.on('disconnect', () => {
-        usersCollection.findOneAndDelete({ id: socket.id })
-          .then((result) => {
-            const user = result.value;
-            if (user) {
-              io.emit('user-removed', user);
-            }
-          })
-          .catch((err) => {
-            console.log('Error removing user from MongoDB:', err);
-          });
-        const index = connectedUsers.findIndex((user) => user.id === socket.id);
-        if (index !== -1) {
-          connectedUsers.splice(index, 1);
-          console.log(`User ${socket.id} connectedUsers`);
-        }
-      });
-    });
-
-    server.listen(PORT, () => {
-      console.log(`The server is Listening on http://localhost:${PORT} \nPID: ${PID}\n`);
-    });
-  })
-  .catch((err) => {
-    console.log('Error connecting to MongoDB:', err);
+  // When the user sends their data to the server
+  socket.on('user_data', data => {
+    const user = { id: socket.id, ...data };
+    users.push(user);
+    availableUsers[user.gender].push(user);
   });
+
+  // When the user searches for a match
+  socket.on('search', gender => {
+    let match;
+
+    if (gender === 'any') {
+      const genders = ['male', 'female'];
+      const randomGender = genders[Math.floor(Math.random() * genders.length)];
+      match = availableUsers[randomGender].pop();
+    } else {
+      match = availableUsers[gender].pop();
+    }
+
+    if (match) {
+      // Send a "matched" event to both users
+      socket.emit('matched', match);
+      io.to(match.id).emit('matched', user);
+
+      // Remove users from available users
+      const userIndex = availableUsers[user.gender].findIndex(u => u.id === user.id);
+      availableUsers[user.gender].splice(userIndex, 1);
+
+      const matchIndex = availableUsers[match.gender].findIndex(u => u.id === match.id);
+      availableUsers[match.gender].splice(matchIndex, 1);
+    } else {
+      // Set a timeout to wait for a match
+      const timeout = setTimeout(() => {
+        // Send a "no match" event to the user
+        socket.emit('no_match');
+      }, matchTimeout);
+
+      // Store the timeout in the socket object
+      socket.matchTimeout = timeout;
+    }
+  });
+
+  // When the user disconnects from the server
+  socket.on('disconnect', () => {
+    // Remove the user's data from the user data list
+    const index = users.findIndex(u => u.id === socket.id);
+    const user = users[index];
+
+    // Remove the user from the available user list
+    const availableIndex = availableUsers[user.gender].findIndex(u => u.id === user.id);
+    availableUsers[user.gender].splice(availableIndex, 1);
+
+    users.splice(index, 1);
+
+    // Clear the match timeout if it exists
+    if (socket.matchTimeout) {
+      clearTimeout(socket.matchTimeout);
+    }
+    console.log('User disconnected');
+  });
+});
+server.listen(PORT, () => {
+  console.log(`The server is Listening on http://localhost:${PORT} \nPID: ${PID}\n`);
+});
+
