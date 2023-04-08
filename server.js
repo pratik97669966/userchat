2f05fa92fae2646470495658b2a4fc772672b8e1
Pratik Pansare <pratik.p@qupapp.com>
2023-04-09 00:53:25 +0530

    init

--- server.js
+++ server.js
@@ -1,93 +1,36 @@
-const path = require('path');
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server, {
-  pingInterval: 10000,
-  pingTimeout: 5000,
-  maxHttpBufferSize: 1e8,
+var profanity = require("profanity-hindi");
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
 });
-io.sockets.setMaxListeners(5000);
+app.use(express.static("public"));
 
-const PID = process.pid;
-const PORT = process.env.PORT || 5000;
-
-console.log('Deployment Done !!');
-app.use(express.static(path.join(__dirname, 'public')));
-const users = [];
-const availableUsers = {
-  any: [],
-  male: [],
-  female: []
-};
-const matchTimeout = 2 * 60 * 1000; // 2 minutes in milliseconds
-
-io.on('connection', socket => {
-  // When a user connects to the server
-  console.log(`connected ${socket.id} `);
-
-  // When the user sends their data to the server
-  socket.on('user_data', data => {
-    const user = { id: socket.id, ...data };
-    users.push(user);
-    availableUsers[user.gender].push(user);
-  });
-
-  // When the user searches for a match
-  socket.on('search', gender => {
-    let match;
-
-    if (gender === 'any') {
-      const genders = ['male', 'female'];
-      const randomGender = genders[Math.floor(Math.random() * genders.length)];
-      match = availableUsers[randomGender].pop();
-    } else {
-      match = availableUsers[gender].pop();
-    }
-
-    if (match) {
-      // Send a "matched" event to both users
-      socket.emit('matched', match);
-      io.to(match.id).emit('matched', user);
-
-      // Remove users from available users
-      const userIndex = availableUsers[user.gender].findIndex(u => u.id === user.id);
-      availableUsers[user.gender].splice(userIndex, 1);
-
-      const matchIndex = availableUsers[match.gender].findIndex(u => u.id === match.id);
-      availableUsers[match.gender].splice(matchIndex, 1);
-    } else {
-      // Set a timeout to wait for a match
-      const timeout = setTimeout(() => {
-        // Send a "no match" event to the user
-        socket.emit('no_match');
-      }, matchTimeout);
-
-      // Store the timeout in the socket object
-      socket.matchTimeout = timeout;
-    }
-  });
-
-  // When the user disconnects from the server
-  socket.on('disconnect', () => {
-    // Remove the user's data from the user data list
-    const index = users.findIndex(u => u.id === socket.id);
-    const user = users[index];
-
-    // Remove the user from the available user list
-    const availableIndex = availableUsers[user.gender].findIndex(u => u.id === user.id);
-    availableUsers[user.gender].splice(availableIndex, 1);
-
-    users.splice(index, 1);
-
-    // Clear the match timeout if it exists
-    if (socket.matchTimeout) {
-      clearTimeout(socket.matchTimeout);
-    }
-    console.log('User disconnected');
-  });
-});
-server.listen(PORT, () => {
-  console.log(`The server is Listening on http://localhost:${PORT} \nPID: ${PID}\n`);
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
 });
 
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      var isDirty = profanity.isMessageDirty(message);
+      if (isDirty) {
+        message = "<span style='color: red;'>🚨 Using bad word may ban your account permanantly</span>";
+      }
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3030);
